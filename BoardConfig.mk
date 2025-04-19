98e2bb919434c84742dc314efb39c283dbaf53d3
VIKAS9793 <vikassahani17@gmail.com>
2025-04-19 18:38:47 +0530

    giulia: Remove OpenDelta and YAAP configurations

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2024 Yet Another AOSP Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
--- lineage_giulia.mk
+++ lineage_giulia.mk
@@ -8,26 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from giulia device
-$(call inherit-product, device/oneplus/giulia/device.mk)
-
-# Inherit some common LineageOS stuff.
+# Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_giulia
+# Inherit from giulia device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := OnePlus
 PRODUCT_DEVICE := giulia
 PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := CPH2691
-
+PRODUCT_NAME := lineage_giulia
 PRODUCT_SYSTEM_NAME := OnePlus13R
 PRODUCT_SYSTEM_DEVICE := OP5D3BL1
 
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
-
+# Build info
+BUILD_FINGERPRINT := "OnePlus/CPH2691/OP5D3BL1:15/UKQ1.231108.001/T.R4T3.150e0e-1-2:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus13R-user 15 UKQ1.231108.001 T.R4T3.1cff019-3eb5-3eb4 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+    PRODUCT_NAME=$(PRODUCT_SYSTEM_NAME) \
+    PRIVATE_BUILD_DESC="CPH2691-user 15 UKQ1.231108.001 T.R4T3.150e0e-1-2 release-keys"
 
-BUILD_FINGERPRINT := OnePlus/CPH2691IN/OP5D3BL1:15/UKQ1.231108.001/T.R4T3.1cff019-3eb5-3eb4:user/release-keys 
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus 
