88f970f49777e929dbe9cdf3693cfdba90e062dc
VIKAS9793 <vikassahani17@gmail.com>
2025-04-20 21:17:31 +0530

    giulia: Set up common tree inheritance
    * Added inheritance from sm8550-common in device.mk * Added inheritance from sm8550-common in BoardConfig.mk * Fixed copyright year
    
    Signed-off-by: VIKAS9793 <vikassahani17@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,11 @@
 #
-# Copyright (C) 2025 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Inherit from sm8550-common
+include device/oneplus/sm8550-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/oneplus/giulia
 
